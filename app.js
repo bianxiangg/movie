8171fdc3bc28fd0bd931cd8d1c27db30a7144556
bianxiangg <bianxiangg>
2016-09-09 18:00:31 +0800

    第一次提交

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+app.listen(port);
+
+console.log('imooc start on port', port);
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'imooc 首页'
+    })
+});
+
+app.get('/detail/:id', function (req, res) {
+    res.render('index', {
+        title: 'imooc 详情页'
+    })
+});
+
+app.get('/admin/movie', function (req, res) {
+    res.render('index', {
+        title: 'imooc 后台录入页'
+    })
+});
+
+app.get('/admin/list', function (req, res) {
+    res.render('index', {
+        title: 'imooc 列表页'
+    })
+});
