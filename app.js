364b504614bf1a5840bc871d7b5a935a691b5d23
bianxiangg <mymailbox001@163.com>
2016-10-12 22:37:03 +0800

    添加静态文件中间件

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var port = process.env.PORT || 3000;
 
 app.set('views', './views');
 app.set('view engine', 'jade');
-
-app.use(express.static('publics'));
-app.use(express.static('jss'));
+app.use(express.static('js'));
 app.listen(port);
 app.locals.pretty = true;
 console.log('imooc start on port', port);
